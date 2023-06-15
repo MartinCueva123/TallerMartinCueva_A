39d502f5084f8c78c7b6dfdef58c1a2bcd62441e
MartinCueva123 <136726839+MartinCueva123@users.noreply.github.com>
2023-06-15 15:03:31 +0000

    Modulo Martincueva

---
+++ Programa.py
@@ -0,0 +1,16 @@
+from funciones_matematica import sumar, restar, dividir, multiplicar
+
+print(sumar(9,7))
+
+print(restar(9,8))
+
+print(dividir(45,5))
+
+print(multiplicar(20,2))
+
+
+
+from algebra import distancia
+
+print(distancia(7, 8 ,9 ,4))
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# TallerMartinCueva_A
-Taller de entorno de desarrollo
+
+# Taller Nro. 1 Entorno de desarrollo
+
+- Nombre: Martin Cueva
+
+
+## Descripción
+
+## Funcionamiento
+
+![](img/woman-8035772_1280.jpg)
